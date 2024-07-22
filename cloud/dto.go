a1fb2c92a7fed1e294f7858a11f0f35b2d5728fc
thefourCraft <me@thefourcraft.com>
2024-07-22 15:17:45 +0300

    feat: Update data source for retrieving images
    The `dataSourceImage` function in `data_source_image.go` has been updated to improve error handling and provide more informative error messages. The code now checks if a matching image is found and returns an error with a list of available public images if no match is found. Additionally, the error message now includes a link to the documentation for more information on private images.
    
    Note: This commit message assumes that the purpose of the code changes is to update the data source for retrieving images. If there are other changes in the code that are not mentioned in the provided diff, please provide more context so that a more accurate commit message can be generated.

---
+++ cloud/dto.go
@@ -0,0 +1,59 @@
+package cloud
+
+type listServersPostValues struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type createServerPostValues struct {
+	Name             string  `json:"name"`
+	Password         string  `json:"password"`
+	PasswordValidate string  `json:"passwordValidate"`
+	SSHKey           string  `json:"ssh-key"`
+	Datacenter       string  `json:"datacenter"`
+	Image            string  `json:"image"`
+	CPU              string  `json:"cpu"`
+	RAM              float64 `json:"ram"`
+	Disk             string  `json:"disk"`
+	DailyBackup      string  `json:"dailybackup"`
+	Managed          string  `json:"managed"`
+	Network          string  `json:"network"`
+	Quantity         string  `json:"quantity"`
+	BillingCycle     string  `json:"billingcycle"`
+	MonthlyPackage   string  `json:"monthlypackage"`
+	PowerOn          string  `json:"poweronaftercreate"`
+	ScriptFile       string  `json:"script-file"`
+}
+
+type powerOperationServerPostValues struct {
+	ID    string `json:"id"`
+	Force bool   `json:"force"`
+}
+
+type configureServerPostValues struct {
+	ID             string  `json:"id"`
+	CPU            string  `json:"cpu"`
+	RAM            float64 `json:"ram"`
+	DailyBackup    string  `json:"dailybackup"`
+	Managed        string  `json:"managed"`
+	BillingCycle   string  `json:"billingcycle"`
+	MonthlyPackage string  `json:"monthlypackage"`
+}
+
+type changePasswordServerPostValues struct {
+	ID       string `json:"id"`
+	Password string `json:"password"`
+}
+
+type renameServerPostValues struct {
+	ID      string `json:"id"`
+	NewName string `json:"new-name"`
+}
+
+type changeDisksPostValues struct {
+	ID     string `json:"id"`
+	Add    string `json:"add,omitempty"`
+	Remove string `json:"remove,omitempty"`
+	Resize string `json:"resize,omitempty"`
+	Size   string `json:"size,omitempty"`
+}
